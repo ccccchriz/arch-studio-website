a83c486239104ab6d783c1e34592bdd18ffadd29
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-01-19 12:59:33 +0100

    added tablet and desktop styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,7 +34,7 @@ export default function Header({ isExpanded, setIsExpanded }: HeaderProps) {
   });
 
   return (
-    <header className="flex justify-between p-8 font-primary font-bold text-neutral-600">
+    <header className="flex justify-between items-center p-8 font-primary font-bold text-neutral-600 tablet:text-neutral-400 tablet:gap-20 desktop:gap-28">
       <a href="">
         <img
           src="/images/logo.svg"
@@ -47,6 +47,7 @@ export default function Header({ isExpanded, setIsExpanded }: HeaderProps) {
         type="button"
         aria-expanded={isExpanded}
         ref={menuButton}
+        className="tablet:hidden"
       >
         {isExpanded ? (
           <img src="/images/icons/icon-close.svg" alt="close menu" />
@@ -58,10 +59,10 @@ export default function Header({ isExpanded, setIsExpanded }: HeaderProps) {
         ref={nav}
         className={`${
           isExpanded ? "absolute" : "hidden"
-        }  top-24 left-0 w-full h-full bg-neutral-600 bg-opacity-50`}
+        } tablet:block tablet:static top-24 left-0 w-full h-full bg-neutral-600 bg-opacity-50 tablet:bg-opacity-0`}
       >
         <ul
-          className={`bg-neutral-200 list-none px-12 py-10 text-3xl grid gap-4 ml-8 w-[calc(100%-2rem)]`}
+          className={`flex flex-col bg-neutral-200 list-none px-12 py-10 text-3xl gap-4 ml-8 w-[calc(100%-2rem)] tablet:w-auto tablet:p-0 tablet:bg-neutral-100 tablet:flex-row tablet:text-lg tablet:gap-16`}
         >
           <li>
             <a href="" className="block">
