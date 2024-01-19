931bf9e664b358ec1f835f81ff32bf1334ddbdd6
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-01-19 12:29:57 +0100

    added main layout

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,27 @@
+import Header from "../components/Header";
+import { useEffect, useRef, useState } from "react";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  const main = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    if (isExpanded) {
+      main.current!.setAttribute("inert", "");
+    } else {
+      main.current!.removeAttribute("inert");
+    }
+  }, [isExpanded]);
+
+  return (
+    <>
+      <Header isExpanded={isExpanded} setIsExpanded={setIsExpanded}></Header>
+      <main ref={main}>{children}</main>
+    </>
+  );
+}
