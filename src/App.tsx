99aaf0d17c0c9ddf3d513cfb5d6354e6d0774ef4
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-01-21 19:17:26 +0100

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import Portfolio from "./Pages/Portfolio";
+import About from "./Pages/About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/portfolio",
+    element: <Portfolio />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "*",
+    element: <Home />,
+  },
+]);
 
 function App() {
-  return <Contact />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
